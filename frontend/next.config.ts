778038a7fd7a192ef546df8e123bf544a792d11c
ebifrier <ebifrier@gmail.com>
2024-11-06 23:23:20 +0900

    frontendの環境変数をnext.config.jsで設定するようにした

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  env: {
+    NEXT_TELEMETRY_DISABLED: "1",
+    NEXT_PUBLIC_API_URL:
+      process.env.NODE_ENV === "production"
+        ? "https://backend.sf-jp-wikitool.com"
+        : "http://localhost:5000",
+  },
 };
 
 export default nextConfig;
