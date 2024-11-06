7d3b104dd66dad734dee80cabe6a6c860f95700b
ebifrier <ebifrier@gmail.com>
2024-11-06 23:42:11 +0900

    不要なログ出力を削除した

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps): ReactNode {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       </Head>
       <Component {...pageProps} />;
     </>
--- frontend/src/pages/item.tsx
+++ frontend/src/pages/item.tsx
@@ -22,7 +22,6 @@ import {
 import { TableData } from "@/components/table";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5000";
-console.log(API_URL);
 
 async function fetcher<T>(url: string): Promise<T> {
   return axios.get(`${API_URL}${url}`).then((res) => res.data);
