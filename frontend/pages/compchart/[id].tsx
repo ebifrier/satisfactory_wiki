dcdd84ecd54559bc601206baa2222aa1aa59be9a
m.suzuki <suzuki.igoshogi@gmail.com>
2024-11-16 18:58:00 +0900

    デフォルトレシピ設定用の関数を修正

--- frontend/pages/compchart/[id].tsx
+++ frontend/pages/compchart/[id].tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { useDrop } from "react-dnd";
-import useSWR from "swr";
 import * as Icon from "@heroicons/react/24/outline";
 import {
   TRecipe,
@@ -25,6 +24,7 @@ import {
   IngredientMultiSelect,
 } from "@/components";
 import { actions } from "@/features/compchartSlice";
+import { getDefaultRecipeSels } from "@/features/defaultRecipes";
 
 //
 // 範囲外のドロップエリア
@@ -55,147 +55,6 @@ const OutsideDropArea: React.FC<
   );
 };
 
-// const getDefaultRecipeSels = (recipes: TRecipe[]): TRecipeSelection[] => {
-//   const findRecipe = (recipeId: string): TRecipe => {
-//     return recipes.find((r) => r.id === recipeId)!;
-//   };
-//   const makeRecipes = (recipeIds: string[]): TRecipe[] => {
-//     return recipeIds.map((id) => findRecipe(id));
-//   };
-
-//   return [
-//     {
-//       name: "基本",
-//       recipes: makeRecipes([
-//         "Iron_Ingot",
-//         "Iron_Plate",
-//         "Iron_Rod",
-//         "Screw",
-//         "Reinforced_Iron_Plate",
-//       ]),
-//     },
-//     {
-//       name: "基本 & 鋳造ネジ",
-//       recipes: makeRecipes([
-//         "Cast_Screw",
-//         "Iron_Plate",
-//         "Reinforced_Iron_Plate",
-//       ]),
-//     },
-//     {
-//       name: "ネジ留め鉄板 & 鋳造ネジ",
-//       recipes: makeRecipes(["Bolted_Iron_Plate", "Cast_Screw", "Iron_Plate"]),
-//     },
-//     {
-//       name: "縫合鉄板 & 鉄のワイヤー",
-//       recipes: makeRecipes(["Iron_Plate", "Iron_Wire", "Stitched_Iron_Plate"]),
-//     },
-//     {
-//       name: "縫合鉄板",
-//       recipes: makeRecipes(["Iron_Plate", "Stitched_Iron_Plate", "Wire"]),
-//     },
-//     {
-//       name: "鋼鉄のネジ",
-//       recipes: makeRecipes([
-//         "Steel_Beam",
-//         "Steel_Screw",
-//         "Iron_Plate",
-//         "Reinforced_Iron_Plate",
-//       ]),
-//     },
-//     {
-//       name: "ネジ留め鉄板 & 鋼鉄のネジ",
-//       recipes: makeRecipes([
-//         "Steel_Beam",
-//         "Steel_Screw",
-//         "Iron_Plate",
-//         "Bolted_Iron_Plate",
-//       ]),
-//     },
-
-//     /*{
-//       name: "アルミ通常レシピ",
-//       recipes: makeRecipes([
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Aluminum)",
-//         "Alumina_Solution",
-//         "Aluminum_Scrap",
-//         "Aluminum_Ingot",
-//         "Silica",
-//       ]),
-//     },
-//     {
-//       name: "アルミ代替レシピ",
-//       recipes: makeRecipes([
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Aluminum)",
-//         "Pure_Aluminum_Ingot",
-//         "Electrode_Aluminum_Scrap",
-//         "Sloppy_Alumina",
-//         "Heavy_Oil_Residue",
-//         "Petroleum_Coke",
-//       ]),
-//     },
-//     {
-//       name: "カテリウム基本",
-//       recipes: makeRecipes([
-//         "Caterium_Ingot",
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Caterium)",
-//       ]),
-//     },
-//     {
-//       name: "純カテリウムのインゴット",
-//       recipes: makeRecipes([
-//         "Pure_Caterium_Ingot",
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Caterium)",
-//       ]),
-//     },
-//     {
-//       name: "鉱石変換+アルミ通常レシピ",
-//       recipes: makeRecipes([
-//         "Bauxite_(Caterium)",
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Aluminum)",
-//         "Alumina_Solution",
-//         "Aluminum_Scrap",
-//         "Aluminum_Ingot",
-//         "Silica",
-//       ]),
-//     },
-//     {
-//       name: "鉱石変換+アルミ代替レシピ",
-//       recipes: makeRecipes([
-//         "Bauxite_(Caterium)",
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Aluminum)",
-//         "Pure_Aluminum_Ingot",
-//         "Electrode_Aluminum_Scrap",
-//         "Sloppy_Alumina",
-//         "Heavy_Oil_Residue",
-//         "Petroleum_Coke",
-//       ]),
-//     },
-//     {
-//       name: "鉄基本",
-//       recipes: makeRecipes([
-//         "Iron_Ingot",
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Iron)",
-//       ]),
-//     },
-//     {
-//       name: "純鉄のインゴット",
-//       recipes: makeRecipes([
-//         "Pure_Iron_Ingot",
-//         "Reanimated_SAM",
-//         "Ficsite_Ingot_(Iron)",
-//       ]),
-//     },*/
-//   ];
-// };
-
 //
 // メインコンポーネント
 //
@@ -207,7 +66,8 @@ const CompChartPage: React.FC = () => {
   );
   const dispatch = useAppDispatch();
   const [searchTerm, setSearchTerm] = React.useState<string>("");
-  const { data: recipes } = useSWR<TRecipe[]>("/api/v1/recipes", fetcher);
+  const [recipes, setRecipes] = React.useState<TRecipe[]>([]);
+  const [recipesCompleted, setRecipesCompleted] = React.useState(false);
   const { itemOptions, data: itemsByGroup } = useItemOptions();
   const items = React.useMemo(
     () => itemsByGroup?.map(([, items]) => items)?.flat(),
@@ -219,6 +79,29 @@ const CompChartPage: React.FC = () => {
     [chartData]
   );
 
+  React.useEffect(() => {
+    const getRecipes = async (page: number) => {
+      return await fetcher<TRecipe[]>(`/api/v1/recipes?page=${page}`);
+    };
+
+    const func = async () => {
+      let rs: TRecipe[] = [];
+      for (let page = 0; ; page++) {
+        const partial = await getRecipes(page);
+        rs = rs.concat(partial);
+        setRecipes(rs);
+
+        if (partial.length < 50) {
+          setRecipesCompleted(true);
+          break;
+        }
+      }
+    };
+
+    setRecipesCompleted(false);
+    func();
+  }, []);
+
   const {
     recipeSels = [],
     productAmounts = [],
@@ -226,14 +109,14 @@ const CompChartPage: React.FC = () => {
   } = chart ?? {};
 
   // React.useEffect(() => {
-  //   if (recipes == null) return;
+  //   if (recipes == null || !recipesCompleted) return;
   //   dispatch(
   //     actions.setRecipeSels({
   //       chartId,
   //       recipeSels: getDefaultRecipeSels(recipes),
   //     })
   //   );
-  // }, [dispatch, recipes]);
+  // }, [dispatch, chartId, recipes, recipesCompleted]);
 
   // 検索ワードによるフィルタリング
   const FilteredDraggableRecipes = React.useMemo(() => {
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -23,4 +23,12 @@ export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
+//
+// ブラウザに保存されているキャッシュデータを削除します。
+//
+export const clearPersistedData = async () => {
+  await persistor.flush();
+  persistor.purge();
+};
+
 export default store;
